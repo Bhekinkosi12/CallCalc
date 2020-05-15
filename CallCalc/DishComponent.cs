4dbd88cfa1cf6d1a6bc7da06bf887144813e8b6c
SamheinDM <63172040+SamheinDM@users.noreply.github.com>
2020-05-15 13:34:28 +0300

    Work around logic

---
+++ CallCalc/DishComponent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CallCalc
+{
+    class DishComponent
+    {
+        public double protein;
+        public double fat;
+        public double carbohydrates;
+        public int weight;
+        public DishComponent(int w)
+        {
+            weight = w;
+        }
+        public double TotalCallories(double tcall)
+        {
+            _ = (protein * 4 + fat * 9 + carbohydrates * 4) / 100 * weight;
+            return tcall;
+        }
+        public double DefaultCallories(double call)
+        {
+            _ = protein * 4 + fat * 9 + carbohydrates * 4;
+            return call;
+        }
+    }
+}
