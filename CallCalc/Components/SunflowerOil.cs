62955e337e15f720facdca90b072b0bf9b8b7f66
Максим Перевезенцев <SamheinDM@yandex.ru>
2020-05-16 21:03:13 +0300

    Adding interface and dish class

---
+++ CallCalc/Components/SunflowerOil.cs
@@ -0,0 +1,13 @@
+﻿namespace CallCalc
+{
+    public class SunflowerOil : DishComponent
+    {
+        public SunflowerOil(int w)
+        {
+            weight = w;
+            protein = 0;
+            fat = 99.9;
+            carbohydrates = 0;
+        }
+    }
+}
